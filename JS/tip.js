1066dc751efb34092ee9bb095f463570bf806e0e
Nartyy <129529306+lfantine@users.noreply.github.com>
2023-04-30 00:44:39 +0200

    tip calculator

---
+++ JS/tip.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+	document.querySelector('#but').onclick = calculate;
+}
+
+function calculate(){
+	bill = document.querySelector('#bil').value;
+	tip = document.querySelector('#tips').value;
+	if (bill == "" || tip == "")
+	{
+		document.querySelector('#ress').innerHTML = "Error";
+		return;
+	}
+	else if (bill < 0)
+	{
+		document.querySelector('#ress').innerHTML = "Error: negative Bill";
+		return;
+	}
+	else if (tip < 0)
+	{
+		document.querySelector('#ress').innerHTML = "Error: negative Tip";
+		return;
+	}
+	let res;
+	tip /= 100;
+	tip *= parseInt(bill);
+	res = parseFloat(bill) + parseFloat(tip);
+	alert(res);
+	document.querySelector('#ress').innerHTML = res;
+}
