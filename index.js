ef04ada3a6170ffb8dcc390846cc503df4e44820
Nartyy <129529306+lfantine@users.noreply.github.com>
2023-05-01 16:00:51 +0200

    re home

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function link(id){
 	const obj = document.getElementById(id);
 	obj.style.animation = "link 2s linear infinity";
+	if (id == "github")
+		window.open("https://github.com/Nartyy", "_blank");
+	
 }
