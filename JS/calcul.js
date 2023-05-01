85dbe7a05eeda8b6dc27f4d0dd44cf380838f330
Nartyy <129529306+lfantine@users.noreply.github.com>
2023-05-01 12:45:01 +0200

    calculatrice

--- JS/calcul.js
+++ JS/calcul.js
@@ -1,3 +1,29 @@
+let base = 0;
+const screen = document.getElementById('screen');
+
 window.onload = () => {
 	
+}
+
+function calc(id) {
+	if (id == 'C')
+	{
+		clear();
+		return ;
+	}
+	if (id == '=')
+	{
+		screen.innerHTML = eval(screen.innerHTML);
+		return;
+	}
+	if (screen.innerHTML == "0")
+		screen.innerHTML= "";
+	screen.innerHTML += id;
+
+}
+
+function clear(){
+	screen.innerHTML = "0";
+	base = 0;
+	actual = 0;
 }
